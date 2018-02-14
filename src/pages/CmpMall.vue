32be8926f2a35461472687e6e9308463c7a5c7c5
eriollee <eriollee@163.com>
2018-02-14 12:45:10 +0800

    用transition实现淡入淡出效果

--- src/pages/CmpMall.vue
+++ src/pages/CmpMall.vue
@@ -1,11 +1,22 @@
 <template>
     <div class="vm-image-list">
         <Row class="image-list-heading vm-panel">
-            <VmSearch placeholder="请输入融e购产品编号.." @transfer="getSearchValue"></VmSearch>
+            <!-- <VmSearch placeholder="请输入融e购产品编号.." @transfer="getSearchValue"></VmSearch> -->
+            <button v-on:click="show = !show">
+                Toggle
+            </button>
+            <transition name="fade">
+                <p v-if="show">hello</p>
+            </transition>
         </Row>
-        <Row class="image-list fade-enter-active"  >
-             <VmImageInfo v-show="imageInfoShow" ></VmImageInfo>
-       </Row>
+
+        <Row class="image-list"  >
+            <transition name="fade">
+                <VmImageInfo v-if="imageInfoShow" ></VmImageInfo>
+            </transition>
+        </Row>
+
+
        <!-- <Row>
            <VmImageMall></VmImageMall>
        </Row> -->
@@ -39,9 +50,18 @@
     },
     data: function () {
       return {
+           show: true,
           imageInfoShow:false,
           imageMallShow:false
       }
     }
   }
 </script>
+ <style type="text/css" rel="stylesheet">
+    .fade-enter-active, .fade-leave-active {
+    transition: opacity .5s;
+    }
+    .fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
+    opacity: 0;
+    }
+</style>
